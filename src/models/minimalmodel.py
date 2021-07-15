0b3c16505a8aad14979539603623959c54d58ec5
federico.almaviva@studenti.unimi.it <federico.almaviva@studenti.unimi.it>
2021-07-15 11:47:05 +0200

    remeake di minimalModel & cleanup

--- src/models/minimalmodel.py
+++ src/models/minimalmodel.py
@@ -15,6 +15,119 @@ from models.vae_mnist import MNIST
 from models.vae_svhn import SVHN
 from mymodel import MNIST_SVHN
 
+
+class MinimalModel(nn.Module):
+    def __init__(self, params):
+        super(MinimalModel, self).__init__()
+        self.pz = dist.Laplace
+        self.vaes = nn.ModuleList([MNIST(params), SVHN(params)])
+        self.modelName = None  # filled-in per sub-class
+        self.params = params
+        grad = {'requires_grad': params.learn_prior}
+        self._pz_params = nn.ParameterList([
+            nn.Parameter(torch.zeros(1, params.latent_dim), requires_grad=False),  # mu
+            nn.Parameter(torch.zeros(1, params.latent_dim), **grad)  # logvar
+        ])
+        self.vaes[0].llik_scaling = prod(self.vaes[1].dataSize) / prod(self.vaes[0].dataSize) \
+            if params.llik_scaling == 0 else params.llik_scaling
+
+    @property
+    def pz_params(self):
+        return self._pz_params[0], F.softmax(self._pz_params[1], dim=1) * self._pz_params[1].size(-1)
+
+    def getDataLoaders(self,batch_size, shuffle=True, device="cuda"):
+        if not (os.path.exists('../data/train-ms-mnist-idx.pt')
+                and os.path.exists('../data/train-ms-svhn-idx.pt')
+                and os.path.exists('../data/test-ms-mnist-idx.pt')
+                and os.path.exists('../data/test-ms-svhn-idx.pt')):
+            raise RuntimeError('Generate transformed indices with the script in bin')
+        # get transformed indices
+        t_mnist = torch.load('../data/train-ms-mnist-idx.pt')
+        t_svhn = torch.load('../data/train-ms-svhn-idx.pt')
+        s_mnist = torch.load('../data/test-ms-mnist-idx.pt')
+        s_svhn = torch.load('../data/test-ms-svhn-idx.pt')
+
+        # load base datasets
+        t1, s1 = self.vaes[0].getDataLoaders(batch_size, shuffle, device)
+        t2, s2 = self.vaes[1].getDataLoaders(batch_size, shuffle, device)
+
+        train_mnist_svhn = TensorDataset([
+            ResampleDataset(t1.dataset, lambda d, i: t_mnist[i], size=len(t_mnist)),
+            ResampleDataset(t2.dataset, lambda d, i: t_svhn[i], size=len(t_svhn))
+        ])
+        test_mnist_svhn = TensorDataset([
+            ResampleDataset(s1.dataset, lambda d, i: s_mnist[i], size=len(s_mnist)),
+            ResampleDataset(s2.dataset, lambda d, i: s_svhn[i], size=len(s_svhn))
+        ])
+
+        kwargs = {'num_workers': 2, 'pin_memory': True} if device == 'cuda' else {}
+        train = DataLoader(train_mnist_svhn, batch_size=batch_size, shuffle=shuffle, **kwargs)
+        test = DataLoader(test_mnist_svhn, batch_size=batch_size, shuffle=shuffle, **kwargs)
+        return train, test
+
+    def forward(self, x, K=1):
+        qz_xs, zss = [], []
+        # initialise cross-modal matrix
+        px_zs = [[None for _ in range(len(self.vaes))] for _ in range(len(self.vaes))]
+        for m, vae in enumerate(self.vaes):
+            qz_x, px_z, zs = vae(x[m], K=K)
+            qz_xs.append(qz_x)
+            zss.append(zs)
+            px_zs[m][m] = px_z  # fill-in diagonal
+        for e, zs in enumerate(zss):
+            for d, vae in enumerate(self.vaes):
+                if e != d:  # fill-in off-diagonal
+                    px_zs[e][d] = vae.px_z(*vae.dec(zs))
+        return qz_xs, px_zs, zss
+
+
+
+
+    def generate(self, runPath, epoch):
+        N=64
+        self.eval()
+        with torch.no_grad():
+            data = []
+            pz = self.pz(*self.pz_params)
+            latents = pz.rsample(torch.Size([N]))
+            for d, vae in enumerate(self.vaes):
+                px_z = vae.px_z(*vae.dec(latents))
+                data.append(px_z.mean.view(-1, *px_z.mean.size()[2:]))
+
+        for r, data in enumerate(data):
+            for o, recon in enumerate(data):
+                _data = data[r][:8].cpu()
+                recon = recon.squeeze(0).cpu()
+                # resize mnist to 32 and colour. 0 => mnist, 1 => svhn
+                _data = _data if r == 1 else resize_img(_data, self.vaes[1].dataSize)
+                recon = recon if o == 1 else resize_img(recon, self.vaes[1].dataSize)
+                comp = torch.cat([_data, recon])
+                save_image(comp, '{}/recon_{}x{}_{:03d}.png'.format(runPath, r, o, epoch))
+
+
+
+    def reconstruct(self, data,runPath, epoch):
+        self.eval()
+        with torch.no_grad():
+            _, px_zs, _ = self.forward(data)
+            # cross-modal matrix of reconstructions
+            recons = [[get_mean(px_z) for px_z in r] for r in px_zs]
+
+        for r, recons_list in enumerate(recons):
+            for o, recon in enumerate(recons_list):
+                _data = data[r][:8].cpu()
+                recon = recon.squeeze(0).cpu()
+                # resize mnist to 32 and colour. 0 => mnist, 1 => svhn
+                _data = _data if r == 1 else resize_img(_data, self.vaes[1].dataSize)
+                recon = recon if o == 1 else resize_img(recon, self.vaes[1].dataSize)
+                comp = torch.cat([_data, recon])
+                save_image(comp, '{}/recon_{}x{}_{:03d}.png'.format(runPath, r, o, epoch))
+
+def resize_img(img, refsize):
+    return F.pad(img, (2, 2, 2, 2)).expand(img.size(0), *refsize)
+
+
+'''
 class MinimalModel(nn.Module):
     def __init__(self,params):
         super(MinimalModel, self).__init__()
@@ -91,3 +204,5 @@ class MinimalModel(nn.Module):
             save_image(samples,
                        '{}/gen_samples_{}.png'.format(runPath, i),
                        nrow=int(sqrt(N)))
+
+'''
