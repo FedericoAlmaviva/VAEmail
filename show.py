ac1f59cf009a9a28c0f2f35330f493e5f9344f12
federico.almaviva@studenti.unimi.it <federico.almaviva@studenti.unimi.it>
2021-08-24 10:42:04 +0200

    test token

--- show.py
+++ show.py
@@ -15,22 +15,25 @@ def parseLogEpochs(filepth):
     return ret
 
 
-def showLoss(inputs):
+def showLoss(inputs,showAnnealing=False):
     for infile in inputs:
         filepth = infile.name
         print(filepth)
         data = parseLogEpochs(filepth)
         x = [float(elem[0]) for elem in data]
         y = [float(elem[1]) for elem in data]
-        maxy = max(y)
-        af = [float(elem[2]) * maxy for elem in data]
+        if showAnnealing:
+            maxy = max(y)
+            af = [float(elem[2]) * maxy for elem in data]
         cl = [random.random() for x in range(3)]
         plt.plot(x, y, color=cl)
-        plt.plot(x, af, linestyle='dashed', color=cl)
+        if showAnnealing:
+            plt.plot(x, af, linestyle='dashed', color=cl)
     lgnd_lst = list()
     for e in inputs:
         lgnd_lst += [e.name.split("/")[0]]
-        lgnd_lst += ["annealing factor"]
+        if showAnnealing:
+            lgnd_lst += ["annealing factor"]
 
     plt.legend(lgnd_lst)
     plt.ylabel = "Loss"
@@ -46,7 +49,10 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--loss', action='store_true', default=False, help='enables CUDA training [default: False]')
+    parser.add_argument('--loss', action='store_true', default=False, help='show loss epoch by epoch')
+    parser.add_argument('--detailedLoss', action='store_true', default=False, help='show loss with more granurality')
+    parser.add_argument('--showAnnealing', action='store_true', default=False, help='show loss with more granurality')
     parser.add_argument('--inputs', type=argparse.FileType('r'), nargs='+')
     args = parser.parse_args()
-    showLoss(args.inputs)
+    print(args.detailedLoss)
+    showLoss(args.inputs,showAnnealing=args.showAnnealing)
